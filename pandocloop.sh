5ce9ca415e73c00f8dae7dc5a897116d795665e3
Melanie Cheung <cpy.melanie@gmail.com>
2021-03-13 22:19:06 +0000

    pandoc shell script

---
+++ pandocloop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /Users/melaniecheung/Documents/pandoc-test
+
+created_time=$(date +"%Y%m%d_%H%M%p")
+mkdir $created_time
+
+for x in *.docx
+do
+        x2=${x%%.docx}.md
+        Pandoc --extract-media=. $x
+        Pandoc -f docx -t markdown $x -o $x2
+done
+
+mv *.md $created_time
+mv media $created_time
